20c91873b1a483e18537de57e87601eb810951b9
nickzaf95 <nicholaszafiriou@gmail.com>
2020-05-18 15:51:00 +0200

    create families

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,16 @@
 class FamiliesController < ApplicationController
+
+    def create_family
+        family = Family.new(name: params[:name], code: params[:code])
+        user = User.find_by(username: params[:username])
+
+        if user
+            family.save
+            Inclusion.create(user: user, family: family)
+            render json: { message: "Success! Family created."}
+        else
+            render json: { error: "Please sign in before creating a family" }
+        end
+    end
+
 end
